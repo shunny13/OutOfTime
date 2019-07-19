ad117d4a5c1c923010767651924be4c209a2767d
Nutu <nutu13@hotmail.fr>
2019-07-20 00:58:30 +0200

    again

--- ennemy.py
+++ ennemy.py
@@ -1,8 +1,8 @@
 import pygame
 
+
 class Ennemy:
-    imgs = []
-    def __init__(self,x,y,width,height):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
@@ -13,28 +13,28 @@ class Ennemy:
         self.path = []
         self.img = None
 
-    def draw(self,win):
+    def draw(self, win):
         """
         Draws ennemy with given images
         :param win: surface
         :return: None
         """
-        self.animation_count+=1
+        self.animation_count += 1
         self.img = self.imgs[self.animation_count]
         if self.animation_count >= len(self.imgs):
             self.animation_count = 0
         win.blit(self.img, (self.x, self.y))
         self.move()
 
-    def collide(self,X,Y):
+    def collide(self, X, Y):
         """
         Return if position has hit the Ennemy
         :param x:  init
         :par
         """
         if (X <= self.x + self.width and self.X >= self.x):
-            if(Y<=self.y + self.height and Y >= self.y):
-                return True  
+            if(Y <= self.y + self.height and Y >= self.y):
+                return True
 
     def move(self):
         pass
@@ -43,6 +43,6 @@ class Ennemy:
         """
         Returns if ennemy died and return true if it's the case
         """
-        self.health-=1
-        if self.health<=0:
+        self.health -= 1
+        if self.health <= 0:
             return True
